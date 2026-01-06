23a331bee3256080175f87936bda775a36c47074
Giovani Vasconcelos <giovani@semedcastanhal.pa.gov.br>
2026-01-05 23:23:54 -0300

    feat: Add Vite build configuration for chunk size warning limit and vendor chunking.

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,18 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, 'src'),
       }
+    },
+    build: {
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
     }
   };
 });
